350904d1b49e67a43ba36760485f9bf79b040e45
ivanstiv <albertduf@mail.ru>
2025-05-30 16:33:54 +0300

    dsa

---
+++ spec/ipgeobase_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Ipgeobase do
+  it "has a version number" do
+    expect(Ipgeobase::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
